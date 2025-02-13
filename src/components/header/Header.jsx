13f554f3fe49e933a735019b39f853d3a0149a3d
Juan Camilo Rosero <128432617+juan-camilo-rosero@users.noreply.github.com>
2025-02-13 08:15:00 -0500

    Update Header.jsx

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
     <header className='w-screen h-[10vh] flex fixed top-0 items-center justify-center px-8 border-b-2 border-black bg-primary z-20'>
         <a href='/' className='flex items-center gap-4'>
             <figure className='w-8 h-8 bg-secondary rounded-full'/>
-            <h1 className='text-xl font-semibold text-secondary'>Red Velvet</h1>
+            <h1 className='text-2xl font-semibold text-secondary font-great-vibes'>Red Velvet</h1>
         </a>
     </header>
   )
